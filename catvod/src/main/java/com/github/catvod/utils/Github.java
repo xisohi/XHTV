1e6852a977632a44297af1e7069e110f605df8ec
xihi <xuhui0607@gmail.com>
2024-06-13 21:08:58 +0800

    Update Github.java

--- catvod/src/main/java/com/github/catvod/utils/Github.java
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -8,18 +8,18 @@ import java.io.File;
 
 public class Github {
 
-    public static final String URL = "http://666.ewwe.gq/fongmi/release/main";
+    //public static final String URL = "http://666.ewwe.gq/fongmi/release/main";
 
     private static String getUrl(String path, String name) {
-        return URL + "/" + path + "/" + name;
+        return path + "/" + name;
     }
 
     public static String getJson(String name) {
-        return getUrl("apk/kitkat", name + ".json");
+        return getUrl("https://xhys.lcjly.cn", name + "leanback.json");
     }
 
     public static String getApk(String name) {
-        return getUrl("apk/kitkat", name + ".apk");
+        return getUrl("https://mirror.ghproxy.com/https://github.com/xisohi/TVBoxOSC/releases/download/XHTV", name + ".apk");
     }
 
     public static String getSo(String url) {
