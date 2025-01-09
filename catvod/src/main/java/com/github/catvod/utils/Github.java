c1ac94ef0b91a4702512a9f6fdd6bba7d5c67fa9
xihi <xuhui0607@gmail.com>
2025-01-09 17:14:42 +0800

    更新第一版

--- catvod/src/main/java/com/github/catvod/utils/Github.java
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -2,17 +2,17 @@ package com.github.catvod.utils;
 
 public class Github {
 
-    public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
+//    public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
 
     private static String getUrl(String path, String name) {
-        return URL + "/" + path + "/" + name;
+        return path + "/" + name;
     }
 
     public static String getJson(boolean dev, String name) {
-        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".json");
+        return getUrl("https://xhys.lcjly.cn/update" , "main.json");
     }
 
     public static String getApk(boolean dev, String name) {
-        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".apk");
+        return getUrl("https://mirror.ghproxy.com/https://github.com/xisohi/TVBoxOSC/releases/download/main", name + ".apk");
     }
 }
