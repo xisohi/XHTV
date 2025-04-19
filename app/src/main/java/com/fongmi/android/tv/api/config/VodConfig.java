949610c0f6d4fb198da3de903e1b78778d075729
xisohi <xisohi@gmail.com>
2025-04-19 15:03:09 +0800

    调整源地址

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -165,7 +165,7 @@ public class VodConfig {
     }
 
     private void loadConfigFromDefaultUrl(Callback callback) {
-        String defaultUrl = "http://lcjly.cn/tvbox/XHYSyuan.json";
+        String defaultUrl = "http://xhys.lcjly.cn/XHYSyuan.json";
         try {
             String json = Decoder.getJson(defaultUrl);
             if (!TextUtils.isEmpty(json)) {
