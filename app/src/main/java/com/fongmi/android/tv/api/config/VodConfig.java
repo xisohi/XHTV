8d4b5a93a22e4cb9164f72fcd4617440fb9ab6a5
xisohi <xisohi@gmail.com>
2025-05-16 20:19:09 +0800

    修复已知bug

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -297,7 +297,7 @@ public class VodConfig {
     }
 
     public void setHeaders(List<JsonElement> items) {
-        OkHttp.requestInterceptor().setHeaders(items);
+        OkHttp.responseInterceptor().setHeaders(items);
     }
 
     public List<String> getFlags() {
