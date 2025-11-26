734166276540e20c08092c9d63c7647225f2e26f
xihi <xuhui0607@gmail.com>
2025-11-26 14:40:42 +0800

    update

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,10 +2,10 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    public static final String URL = "https://xhys.lcjly.cn/update";
+    public static final String URL = "https://xhys.lcjly.cn";
 
     private static String getUrl(String name) {
-        return URL + "/apk/" + name;
+        return URL + "/update/" + name;
     }
 
     public static String getJson(String name) {
