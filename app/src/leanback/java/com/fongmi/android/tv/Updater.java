b83cea881bfde514274f3826325f84763750b616
xihi <xuhui0607@gmail.com>
2025-11-26 17:51:56 +0800

    Update Updater.java

--- app/src/leanback/java/com/fongmi/android/tv/Updater.java
+++ app/src/leanback/java/com/fongmi/android/tv/Updater.java
@@ -1,6 +1,7 @@
 package com.fongmi.android.tv;
 
 import android.app.Activity;
+import android.util.Log;  // 添加日志导入
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -22,6 +23,7 @@ import java.io.File;
 import java.util.Locale;
 
 public class Updater implements Download.Callback {
+    private static final String TAG = "Updater"; // 日志标签
 
     private DialogUpdateBinding binding;
     private final Download download;
@@ -32,11 +34,15 @@ public class Updater implements Download.Callback {
     }
 
     private String getJson() {
-        return Github.getJson(lkys);           //示例: https://xhys.lcjly.cn/update/lkys.json
+        String url = Github.getJson("lkys");  // 示例: https://xhys.lcjly.cn/update/lkys.json
+        Log.d(TAG, "JSON请求地址: " + url);  // 打印JSON地址
+        return url;
     }
 
     private String getApk() {
-        return Github.getApk(BuildConfig.FLAVOR_mode + "-" + BuildConfig.FLAVOR_abi);
+        String url = Github.getApk(BuildConfig.FLAVOR_mode + "-" + BuildConfig.FLAVOR_abi);
+        Log.d(TAG, "APK下载地址: " + url);  // 打印APK地址
+        return url;
     }
 
     public static Updater create() {
@@ -44,7 +50,9 @@ public class Updater implements Download.Callback {
     }
 
     public Updater() {
-        this.download = Download.create(getApk(), getFile());
+        String apkUrl = getApk();  // 获取APK地址
+        Log.i(TAG, "初始化下载器，APK地址: " + apkUrl);  // 打印APK地址
+        this.download = Download.create(apkUrl, getFile());
     }
 
     public Updater force() {
@@ -60,17 +68,33 @@ public class Updater implements Download.Callback {
 
     public void start(Activity activity) {
         if (!Setting.getUpdate()) return;
+        Log.i(TAG, "开始检查更新...");  // 添加日志
         App.execute(() -> doInBackground(activity));
     }
 
     private void doInBackground(Activity activity) {
         try {
-            JSONObject object = new JSONObject(OkHttp.string(getJson()));
+            String jsonUrl = getJson();  // 获取JSON地址
+            Log.i(TAG, "正在请求JSON: " + jsonUrl);
+
+            String jsonContent = OkHttp.string(jsonUrl);
+            Log.d(TAG, "JSON返回内容:\n" + jsonContent);  // 打印JSON内容
+
+            JSONObject object = new JSONObject(jsonContent);
             String name = object.optString("name");
             String desc = object.optString("desc");
             int code = object.optInt("code");
-            if (code > BuildConfig.VERSION_CODE) App.post(() -> show(activity, name, desc));
+
+            Log.d(TAG, "解析JSON - name: " + name + ", versionCode: " + code + ", desc: " + desc);
+
+            if (code > BuildConfig.VERSION_CODE) {
+                Log.i(TAG, "发现新版本，当前版本: " + BuildConfig.VERSION_CODE + ", 最新版本: " + code);
+                App.post(() -> show(activity, name, desc));
+            } else {
+                Log.i(TAG, "当前已是最新版本: " + BuildConfig.VERSION_CODE);
+            }
         } catch (Exception e) {
+            Log.e(TAG, "更新检查失败: " + e.getMessage(), e);  // 错误日志
             e.printStackTrace();
         }
     }
@@ -96,6 +120,7 @@ public class Updater implements Download.Callback {
 
     private void confirm(View view) {
         view.setEnabled(false);
+        Log.i(TAG, "用户确认更新，开始下载...");  // 添加日志
         download.start(this);
     }
 
@@ -109,16 +134,21 @@ public class Updater implements Download.Callback {
     @Override
     public void progress(int progress) {
         binding.confirm.setText(String.format(Locale.getDefault(), "%1$d%%", progress));
+        if (progress % 10 == 0) {  // 每10%打印一次，避免日志过多
+            Log.d(TAG, "下载进度: " + progress + "%");
+        }
     }
 
     @Override
     public void error(String msg) {
+        Log.e(TAG, "下载失败: " + msg);  // 错误日志
         Notify.show(msg);
         dismiss();
     }
 
     @Override
     public void success(File file) {
+        Log.i(TAG, "下载成功，文件路径: " + file.getAbsolutePath());  // 成功日志
         FileUtil.openFile(file);
         dismiss();
     }
