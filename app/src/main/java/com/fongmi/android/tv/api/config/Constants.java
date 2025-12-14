1a8d1f9ddca8c5747a293f644b33f3db21681022
xihi <xuhui0607@gmail.com>
2025-12-14 11:19:51 +0800

    Update Constants.java

--- app/src/main/java/com/fongmi/android/tv/api/config/Constants.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/Constants.java
@@ -1,6 +1,6 @@
 package com.fongmi.android.tv.api.config;
 
 public class Constants {
-    public static final String BUILTIN_URL = "https://xhys.xisohi.dpdns.org/XHYSyuan.json"; // ✅ 真实地址
+    public static final String BUILTIN_URL = "https://xhys.xisohi.dpdns.org/ysy.json"; // ✅ 真实地址
     public static final String BUILTIN_NAME = "星辉工作室";
 }
